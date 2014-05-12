93c86b4ec08ee254e53cee56e08e9d31651cfa22
david <d05660@gmail.com>
2014-05-12 19:42:24 +0800

    add esxi55 support

--- setup_centos_pxe.py
+++ setup_centos_pxe.py
@@ -6,8 +6,7 @@ import pxe
 from pxe_data import mac
 
 def main(ip):
-    mac_name = '01-'+'-'.join(mac[ip].lower().split(":"))
-    pxe.setup_centos_pxe('pxe.cfg', mac_name, 'centos', '172.16.1.'+ip, 'centos'+ip)
+    pxe.setup_centos_pxe('pxe.cfg', mac[ip], 'centos', '172.16.1.'+ip, 'centos'+ip)
 
 if __name__ == "__main__":
-    main('130')
+    main(sys.argv[1])
--- setup_esxi_pxe.py
+++ setup_esxi_pxe.py
@@ -6,7 +6,7 @@ import pxe
 from pxe_data import mac
 
 def main(ip):
-    pxe.setup_xen_pxe('pxe.cfg', mac[ip], 'xenserver', '172.16.1.'+ip, 'xenserver'+ip)
+    pxe.setup_esxi_pxe('pxe.cfg', mac[ip], 'esxi55', '172.16.1.'+ip, 'esxi'+ip)
 
 if __name__ == "__main__":
     main(sys.argv[1])
