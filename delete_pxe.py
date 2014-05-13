b66f3cfd807942b3ecbb2f91b368c045be734779
david <d05660@gmail.com>
2014-05-13 09:48:50 +0800

    fix bug#5

--- delete_pxe.py
+++ delete_pxe.py
@@ -7,7 +7,7 @@ from pxe_data import mac
 
 def main(ip):
     mac_name = '01-'+'-'.join(mac[ip].lower().split(":"))
-    pxe.delete_xen_pxe('pxe.cfg', mac_name)
+    pxe.delete_pxe('pxe.cfg', mac_name)
 
 if __name__ == "__main__":
     main(sys.argv[1])
