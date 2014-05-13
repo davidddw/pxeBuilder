2f649bfaa5a9c866474bc021e849fe79ad91d068
david <d05660@gmail.com>
2014-05-13 10:27:50 +0800

    add centos esxi support by david

--- pxe.py
+++ pxe.py
@@ -42,26 +42,64 @@ def generate_file_from_temp(srcdir,srcfile, destdir, destfile, **karg):
     with open(destfilename, "w") as f:
         f.write(content.substitute(**karg))
         
-def setup_pxe(filename, mac_name, m_type, ipaddr, hostname):
+def setup_xen_pxe(filename, mac, m_type, ipaddr, hostname):
     gl_config = get_config_from_cfg(filename, 'global')
+    mac_name = '01-'+'-'.join(mac.lower().split(":"))
     answer = mac_name+'.xml'
     config = get_config_from_cfg(filename, m_type)
     config.update({'answerfile':answer})
     config.update({'xen_ipaddr':ipaddr})
     config.update({'xen_hostname':hostname})
-    generate_file_from_temp(gl_config['temp_folder'], 'pxelinux.in', 
+    config.update(**gl_config)
+    generate_file_from_temp(gl_config['temp_folder'], 'xen_pxe.in', 
                             gl_config['pxelinux'], mac_name, **config)
     print("Info: generate %s in %s" % (mac_name, gl_config['pxelinux']))
     generate_file_from_temp(gl_config['temp_folder'], m_type+'.in', 
-                            config['nfs_ans'], answer, **config)
-    print("Info: generate %s in %s" % (answer, config['nfs_ans']))
+                            gl_config['nfs_ans'], answer, **config)
+    print("Info: generate %s in %s" % (answer, gl_config['nfs_ans']))
     
-def delete_pxe(filename, mac_name, m_type):
+def setup_centos_pxe(filename, mac, m_type, ipaddr, hostname):
     gl_config = get_config_from_cfg(filename, 'global')
-    answer = mac_name+'.xml'
+    mac_name = '01-'+'-'.join(mac.lower().split(":"))
+    answer = mac_name+'.ks'
     config = get_config_from_cfg(filename, m_type)
+    config.update(**gl_config)
+    config.update({'answerfile':answer})
+    config.update({'host_ipaddr':ipaddr})
+    config.update({'hostname':hostname})
+    generate_file_from_temp(gl_config['temp_folder'],  m_type+'_pxe.in', 
+                            gl_config['pxelinux'], mac_name, **config)
+    print("Info: generate %s in %s" % (mac_name, gl_config['pxelinux']))
+    generate_file_from_temp(gl_config['temp_folder'], m_type + '.in', 
+                            gl_config['nfs_ans'], answer, **config)
+    print("Info: generate %s in %s" % (answer, gl_config['nfs_ans']))
+    
+def setup_esxi_pxe(filename, mac, m_type, ipaddr, hostname):
+    gl_config = get_config_from_cfg(filename, 'global')
+    mac_name = '01-'+'-'.join(mac.lower().split(":"))
+    answer = mac_name +'.ks'
+    config = get_config_from_cfg(filename, m_type)
+    config.update(**gl_config)
+    config.update({'ksfile':answer})
+    config.update({'macaddr':mac})
+    config.update({'host_ipaddr':ipaddr})
+    config.update({'hostname':hostname})
+    generate_file_from_temp(gl_config['temp_folder'],  m_type+'_pxe.in', 
+                            gl_config['pxelinux'], mac_name, **config)
+    print("Info: generate %s in %s" % (mac_name, gl_config['pxelinux']))
+    generate_file_from_temp(gl_config['temp_folder'], m_type + '.in', 
+                            gl_config['nfs_ans'], answer, **config)
+    print("Info: generate ks: %s in %s" % (answer, gl_config['nfs_ans']))
+    
+def delete_pxe(filename, mac, m_type):
+    gl_config = get_config_from_cfg(filename, 'global')
+    mac_name = '01-'+'-'.join(mac.lower().split(":"))
+    if m_type=='xenserver' or m_type=='xcp':
+        answer = mac_name+'.xml'
+    else:
+        answer = mac_name+'.ks'
     mac_file = os.path.join(gl_config['pxelinux'], mac_name)
-    answer_file = os.path.join(config['nfs_ans'], answer)
+    answer_file = os.path.join(gl_config['nfs_ans'], answer)
     if os.path.isfile(mac_file):
         os.remove(mac_file)
         print("Info: remove %s" % mac_file)
@@ -104,7 +142,7 @@ def main(argv=None):
             parser.print_help()
             sys.exit()
         else:
-            setup_pxe(opts.file, mac, opts.type, opts.ip, opts.hostname)
+            setup_xen_pxe(opts.file, mac, opts.type, opts.ip, opts.hostname)
 
     except Exception, e:
         indent = len(program_name) * " "
---
+++ setup_xen_pxe.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+import sys
+import pxe
+from pxe_data import mac
+
+def main(ip):
+    pxe.setup_xen_pxe('pxe.cfg', mac[ip], 'xenserver', '172.16.1.'+ip, 'xenserver'+ip)
+
+if __name__ == "__main__":
+    main(sys.argv[1])
